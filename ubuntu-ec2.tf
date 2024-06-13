5eb67006be3d63b0b256d6d950e6f4477b74b8eb
nageshkumar13 <nageshkumar1925@gmail.com>
2024-06-13 14:45:50 +0530

    changes to devops-evn.sh file

--- ubuntu-ec2.tf
+++ ubuntu-ec2.tf
@@ -3,7 +3,14 @@ resource "aws_instance" "devops-ready-ec2"{
     instance_type = var.instance_type
     user_data = file("devops-env.sh")
     iam_instance_profile = aws_iam_instance_profile.devops-ec2-profile.name
+    
     tags = {
-    Name = "devops-ready-ubuntu-machine"
-  }    
+      Name = "devops-ready-ubuntu-machine"
+      Environment = "dev"
+      Project = "operations"
+      Create_date_time = formatdate("YYYY-MM-DD hh:mm:ss ZZZ", timestamp())
+      Team = "double-disgits"      
+    }
+    lifecycle { ignore_changes = [tags["Create_date_time"] ]}
+
 }
